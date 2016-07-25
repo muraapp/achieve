03f12e924942c56e4c5c8e9ae024c8fc5d75ee60
Kazu Murata <muraapp@Kazu-no-MacBook-Pro.local>
2016-07-25 19:06:15 +0900

    dive11 task

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,11 @@ class NoticeMailer < ApplicationMailer
     mail to: "bergsonisme@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
   end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: @contact.email,
+        subject: '【Achieve】お問い合わせを承りました！'
+  end
 end
