80ca421ad3b93eb2fa8ddd0124a2b38976f3e1bb
muraapp <bergsonisme@gmail.com>
2016-08-28 15:13:21 +0900

    dive15 finish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,17 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_blog, only: [:edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.all
     @blogs = Blog.order(:created_at).reverse_order
   end
+
+  def show
+    @comment = @blog.comments.build
+    @comments = @blog.comments
+  end
+
 
   def new
     if params[:back]
